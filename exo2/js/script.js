572e36f80c12d539a36f09ffb5bd1cf00034967b
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-05-16 11:28:20 +0200

    Exercise 2 finished

--- exo2/js/script.js
+++ exo2/js/script.js
@@ -1,7 +1,26 @@
-const carre = document.querySelector('.carres'); 
+const carres = document.querySelectorAll('.carre'); 
 const carre5 = document.querySelector('.carre5'); 
 
-carre.addEventListener('click',function() {
-const objectCss = window.getComputedStyle(carre, carre5);
-carre5.objectCss.backgroundColor = carre.objectCss.backgroundColor;
+
+//pour chaque element avec la classe carre on fera carre.forEach
+carres.forEach (carre=> {
+    carre.addEventListener('click', function() {
+        // on recupere le css de lelement carre qui a ete click
+        const objectCss = window.getComputedStyle(carre);
+        //on recupere la couleur du carre 
+        const color = objectCss.getPropertyValue("background-color");
+        //definir la couleur de carre5
+        carre5.style.setProperty("background-color", color);
+        //modifier le contenu de lelement carre5 pour afficher la couleur ercuperer
+        carre5.innerHTML=color;
+})
+
+//recuperer couleur dans une var couleur fond de l'element ou je clique
+
+//recuperer couleur fond 
+
+//modifier la couleur du carre du bas
+//pour modifier il faut recuperer dans une var
+
+
 })
