673ca1799b77949c6d15ca6f49342ee91ea46d36
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-05-17 12:00:56 +0200

    Exercise 4 (not finished) and beginning of the exercise 5

--- exo4/js/script.js
+++ exo4/js/script.js
@@ -1,19 +1,18 @@
+//on  definit les variables
 const wrapper= document.querySelector("#wrapper");
 const icones= document.querySelectorAll(".icone");
 
 
 icones.forEach (icone=> {
     icone.addEventListener('click', function() {
+
+        //changement de couleur du background
         const objectCss = window.getComputedStyle(icone);
         const color = objectCss.getPropertyValue("background-color");
-        wrapper.style.setProperty("background-color", color);
-        wrapper.innerHTML=color;
-})
 
-icone.forEach(icone => {
-    icone.addEventListener('click', function() {
-       icone.classList.toggle('transition')      
-})
-})
-
-})
+        wrapper.style.setProperty("background-color", color); 
+      
+        //transition
+        icone.classList.toggle('transition') 
+       })
+    })
